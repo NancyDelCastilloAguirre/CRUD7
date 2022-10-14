876f13daf4b3cf35a8debd9a1ba21cef912d2d79
NancyDelCastilloAguirre <ndelcastilloa2000@alumno.ipn.mx>
2022-10-14 02:10:41 -0500

    new

--- crud_main/src/crudnodejsmysql.sql
+++ crud_main/src/crudnodejsmysql.sql
@@ -9,7 +9,7 @@ CREATE TABLE customer (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
-  phone VARCHAR(15)
+  time VARCHAR(15)
 );
 
 ALTER USER 'root'@'localhost' identified WITH mysql_native_password BY 'n0m3l0';
